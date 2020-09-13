f67dc0cc3491167c0b0baa26f4de5520a5208d85
William <gopherx@protonmail.ch>
2020-09-13 11:57:06 +0800

    Unfinished

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+// Tutorial: https://jesselawson.org/rust/getting-started-with-rust-by-building-a-tiny-markdown-compiler/
+// Source code: https://gist.github.com/jesselawson/b5dea9e1abdff2d80f207f9dcfd732b8
+
+fn parse_markdown_file(_filename: &str) {
+
+}
+
+fn main() {
+    println!("Hello, world!");
+}
