22ade9739e69216e5c22a9af4b7daaaa04157989
William <gopherx@protonmail.ch>
2021-11-18 21:57:57 +0800

    hello world

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+// See: https://actix.rs/docs/getting-started/
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("hello world")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
