50d5b9491c3bc16d8e3da4afac0e18b1d257a78c
William <gopherx@protonmail.ch>
2021-11-24 23:18:32 +0800

    With tokio-tungstenite

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1:9002";
+    let listener = TcpListener::bind(&addr).await.expect("Can't listen");
+    println!("Listening on: {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        let peer = stream
+            .peer_addr()
+            .expect("connected streams should have a peer address");
+        println!("Peer address: {}", peer);
+
+        tokio::spawn(echo::accept_connection(peer, stream));
+    }
+}
